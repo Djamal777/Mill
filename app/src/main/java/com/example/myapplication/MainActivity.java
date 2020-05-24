73452e375337c165227c0f1519f9c9050c8b50e5
Djamal <dzamalzairbekov@gmail.com>
2020-05-25 02:08:38 +0300

    ыаы

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
+import java.util.Vector;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,15 +21,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 import static java.lang.Thread.sleep;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    int vopr = 1;
+    int vopr = 0;
+    static int idd;
+    int[] money;
     boolean sound;
     Button var_a, var_b, var_c, var_d;
     ImageButton back, call, help, half;
     TextView vopros, summa;
 
-    ProgressBar progressBar;
     public static final String LOG_TAG = "Main_Activity";
 
     private QuestionResponse questions;
@@ -51,29 +53,129 @@ public class MainActivity extends AppCompatActivity {
         half = (ImageButton) findViewById(R.id.imageButton6);
         vopros = (TextView) findViewById(R.id.tekstVoprosa);
         summa = (TextView) findViewById(R.id.summaVoprosa);
+        money = new int[]{500, 1000, 2000, 3000, 5000, 10000, 15000, 25000, 50000, 100000, 200000, 400000, 800000, 1500000, 3000000};
 
-        progressBar = findViewById(R.id.progressBar2);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Menu.class);
+                startActivity(i);
+            }
+        });
 
         QuestionRequest qr = new QuestionRequest();
-
-        qr.count = 2;
-        qr.qType = 3;
-
-        progressBar.setVisibility(ProgressBar.VISIBLE);
+        qr.qType = 1;
+        qr.count = 5;
         new RequestAsync().execute(qr);
+
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //устанавливаю желтую кнопку
+                //проигрываю "otvet"
+                /*try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+                if (v.getId() == idd) {
+                    if (vopr < 15) {
+                        //устанавливаю зеленую кнопку
+                        //проигрываю "pravilno"
+                        //?sleep
+                        if (vopr == 4) {
+                            QuestionRequest qr = new QuestionRequest();
+                            qr.count = 5;
+                            qr.qType = 2;
+                            new RequestAsync().execute(qr);
+                        }
+                        else if (vopr == 9) {
+                            QuestionRequest qr = new QuestionRequest();
+                            qr.count = 5;
+                            qr.qType = 3;
+                            new RequestAsync().execute(qr);
+                        }
+                        else {
+                            summa.setText(vopr+1 + ". " + money[vopr]);
+                            vopros.setText(questions.data.get(vopr % 5).question);
+                            Vector vec = new Vector();
+                            vec.add(0);
+                            vec.add(1);
+                            vec.add(2);
+                            vec.add(3);
+                            int rand =0+ (int) (Math.random() * vec.size());
+                            var_a.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+                            if ((int)vec.get(rand) == 0) idd = var_a.getId();
+                            vec.remove(rand);
+                            rand =0+ (int) (Math.random() * vec.size());
+                            var_b.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+                            if ((int)vec.get(rand) == 0) idd = var_b.getId();
+                            vec.remove(rand);
+                            rand =0+ (int) (Math.random() * vec.size());
+                            var_c.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+                            if ((int)vec.get(rand) == 0) idd = var_c.getId();
+                            vec.remove(rand);
+                            rand =0+ (int) (Math.random() * vec.size());
+                            var_d.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+                            if ((int)vec.get(rand) == 0) idd = var_d.getId();
+                            vec.remove(rand);
+                            vopr++;
+                        }
+                    }
+                    else if (vopr == 15) {
+                        //ПАБЕДА
+                        //?sleep
+                        //проигрываю "pobeda"
+                        finish();
+                    }
+                }
+                else {
+                    //устанавливаю красную кнопку
+                    //проигрываю "nepravilno"
+                    //?sleep
+                    finish();
+                }
+            }
+        };
+        var_a.setOnClickListener(listener);
+        var_b.setOnClickListener(listener);
+        var_c.setOnClickListener(listener);
+        var_d.setOnClickListener(listener);
     }
 
     private void OnResponse() {
-        progressBar.setVisibility(ProgressBar.INVISIBLE);
-        vopros.setText(questions.data.get(0).question);
-        var_a.setText(questions.data.get(0).answers.get(0));
-        var_b.setText(questions.data.get(0).answers.get(1));
-        var_c.setText(questions.data.get(0).answers.get(2));
-        var_d.setText(questions.data.get(0).answers.get(3));
+        summa.setText(vopr+1 + ". " + money[vopr]);
+        vopros.setText(questions.data.get(vopr % 5).question);
+        Vector vec = new Vector();
+        vec.add(0);
+        vec.add(1);
+        vec.add(2);
+        vec.add(3);
+        int rand =0+ (int) (Math.random() * vec.size());
+        var_a.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+        if ((int)vec.get(rand) == 0) idd = var_a.getId();
+        vec.remove(rand);
+        rand =0+ (int) (Math.random() * vec.size());
+        var_b.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+        if ((int)vec.get(rand) == 0) idd = var_b.getId();
+        vec.remove(rand);
+        rand =0+ (int) (Math.random() * vec.size());
+        var_c.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+        if ((int)vec.get(rand) == 0) idd = var_c.getId();
+        vec.remove(rand);
+        rand =0+ (int) (Math.random() * vec.size());
+        var_d.setText(questions.data.get(vopr % 5).answers.get((int)vec.get(rand)));
+        if ((int)vec.get(rand) == 0) idd = var_d.getId();
+        vec.remove(rand);
+        vopr++;
     }
 
     private void OnFailure() {
-        progressBar.setVisibility(ProgressBar.INVISIBLE);
         Toast.makeText(this, "Ошибка соединения", Toast.LENGTH_LONG).show();
     }
 
@@ -101,7 +203,6 @@ public class MainActivity extends AppCompatActivity {
                     MainActivity.this.OnFailure();
                 }
             });
-
             return null;
         }
     }
--- app/src/main/java/com/example/myapplication/QuestionData.java
+++ app/src/main/java/com/example/myapplication/QuestionData.java
@@ -6,12 +6,4 @@ public class QuestionData {
     public String question;
     public List<String> answers;
     public Integer id=0;
-
-    /*public QuestionData(String question, String[] answers, Integer id){
-        this.question=question;
-        for(int i=0; i<answers.length;i++){
-            this.answers[i]=answers[i];
-        }
-        this.id = id;
-    }*/
 }
