6db58f7ed03f913a4ecc0f23ae9b2abf601a3395
Djamal777 <56553718+Djamal777@users.noreply.github.com>
2021-04-18 16:06:47 +0300

    Update Loading.java

--- app/src/main/java/com/example/myapplication/Loading.java
+++ app/src/main/java/com/example/myapplication/Loading.java
@@ -18,7 +18,7 @@ public class Loading extends AppCompatActivity {
             public void run() {
                 try {
                     int logoTimer = 0;
-                    while(logoTimer < 5000) {
+                    while(logoTimer < 1000) {
                         sleep(100);
                         logoTimer = logoTimer +100;
                     };
